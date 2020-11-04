63724997a1dceff37b7c3f66068c2ffc1e8f5c7a
masaun <masanoriuno@gmail.com>
2020-11-04 22:38:33 +0900

    Add Custody.sol

---
+++ contracts/Custody.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.6.12;
+pragma experimental ABIEncoderV2;
+
+import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
+
+
+/***
+ * @notice
+ **/
+contract Custody {
+    using SafeMath for uint;
+
+    constructor() public {}
+
+}
