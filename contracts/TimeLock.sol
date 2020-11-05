85e58e83bc1ad5f54c8fd2a2a32f62d5b7fe0a18
masaun <masanoriuno@gmail.com>
2020-11-06 08:45:52 +0900

    Inherit TimeLockStorages.sol into TImeLock.sol

--- contracts/TimeLock.sol
+++ contracts/TimeLock.sol
@@ -1,10 +1,11 @@
 pragma solidity ^0.6.12;
 pragma experimental ABIEncoderV2;
 
-import "./RedemptionToken.sol";
+import { TimeLockStorages } from "./timelock/commons/TimeLockStorages.sol";
 
 import { AggregatorV3Interface } from "./chainlink/AggregatorV3Interface.sol";
 
+import { RedemptionToken } from "./RedemptionToken.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
 
@@ -12,7 +13,7 @@ import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
 /***
  * @notice - This contract that allows a user to send an amount of ERC20 token to a smart contract that takes custody of the asset for a pre-determined amount of time (e.g., 7 days) and issues a redemption token.
  **/
-contract TimeLock {
+contract TimeLock is TimeLockStorages {
     using SafeMath for uint;
 
     uint currentTimelockId;  /// Time lock ID
