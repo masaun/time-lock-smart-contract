5b53f45eb75130030500424844322d52fa492c36
masaun <masanoriuno@gmail.com>
2020-11-05 09:15:16 +0900

    Rename with 'Redemption Token'

--- contracts/RedemptionToken.sol
+++ contracts/RedemptionToken.sol
@@ -11,9 +11,9 @@ import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
 contract RedemptionToken is ERC20 {
     using SafeMath for uint;
 
-    constructor() public ERC20("RedemptionToken", "RDT") {
+    constructor() public ERC20("Redemption Token", "RDT") {
         uint initialSupply = 1e8 * 1e18;  /// Initial Supply amount is 100M
-        address initialTokenHolder = msg.sender;
+        address initialTokenHolder = msg.sender;  /// [Note]: msg.sender is the deployer address
         _mint(initialTokenHolder, initialSupply);    	
     }
     
