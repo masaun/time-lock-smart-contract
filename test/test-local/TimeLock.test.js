2c1381a12f6967d11011d090bf7bfe6a0b12d725
masaun <masanoriuno@gmail.com>
2020-11-06 10:37:47 +0900

    [Truffle test]: Success to pass about 'Current locked period should be 7 days'

--- test/test-local/TimeLock.test.js
+++ test/test-local/TimeLock.test.js
@@ -10,10 +10,24 @@ const web3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:8545'
 let TimeLock = {};
 TimeLock = artifacts.require("TimeLock");
 
+const timeLockABI = TimeLock.abi;
+const timeLockAddr = TimeLock.address;
+let timeLock = new web3.eth.Contract(timeLockABI, timeLockAddr);
+
 
 /***
  * @dev - [Execution]: $ truffle test ./test/test-rinkeby/TimeLock.test.js --network local
  **/
 contract("TimeLock contract", function (accounts) {
+
+	it('Current locked period should be 7 days', async () => {
+        let currentLockedPeriod = await timeLock.methods.lockedPeriod().call();
+        const sevenDays = (60 * 60 * 24) * 7;
+
+        console.log("=== currentLockedPeriod ===", currentLockedPeriod);
+        console.log("=== sevenDays ===", sevenDays);
+
+        assert.equal(currentLockedPeriod, sevenDays, 'Current locked period should be 7 days');
+    });
 
 });
